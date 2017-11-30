0e2caa06cb6a1c4d5bd50aff766d9210ff0ecc4d
Easwari Ramani <eshuprad@gmail.com>
2017-11-30 20:16:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your code in this file!
+function scuberGreetingForFeet(dist){
+  let txt='';
+  if(dist <=400)
+  {
+    txt="This one is on me!";
+  }
+  elseif(dist>2000 && dist <=2500)
+  {
+    txt='I will gladly take your thirty bucks.';
+  }
+  else
+  {
+    txt='No can do.';
+  }
+  return txt;
+};
+function ternaryCheckCity(destn){
+  switch destn
+  {
+    case 'NYC':
+      txt="Ok, sounds good.";
+      break;
+    default:
+      txt="No go.";
+      break;
+  }
+  return txt;
+
+};
+function switchOnCharmFromTip(message){
+
+};
